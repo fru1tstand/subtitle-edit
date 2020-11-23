3b9217463737249e980e31850b847d07ae6578fb
Kodlee Yin <kodlee@fru1t.me>
2020-11-23 09:57:40 -0800

    Created single entrypoint for running tools

---
+++ src/me/fru1t/subedit/SubEdit.java
@@ -0,0 +1,39 @@
+package me.fru1t.subedit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Main method entrypoint for Subtitle Edit Tools.
+ */
+public class SubEdit {
+  private static final String PROGRAMS_OUTPUT = "a: FirstLineSmaller; b: UnformatAndCsv";
+
+  public static void main(String[] args) throws IOException {
+    String programToRun;
+    if (args.length == 1) {
+      programToRun = args[0];
+    } else {
+      System.out.println("Yo, what do you wanna run? BTW, you can just pass this as a CLI arg too");
+      System.out.println(PROGRAMS_OUTPUT);
+
+      // Note: "in" is never closed as we don't want to close the underlying System.in stream
+      Scanner in = new Scanner(System.in);
+      programToRun = in.nextLine();
+    }
+
+    switch (programToRun) {
+      case "a":
+        FirstLineSmaller.INSTANCE.run();
+        break;
+      case "b":
+        UnformatAndCsv.INSTANCE.run();
+        break;
+      default:
+        System.out.println("Sorry, I couldn't understand <" + programToRun + ">. Please choose from the following:");
+        System.out.println(PROGRAMS_OUTPUT);
+        break;
+    }
+  }
+}
